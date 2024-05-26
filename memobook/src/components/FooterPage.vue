e684e4234448c5aefe4d36db27b8a8ca5c1a34e0
GabrielMoreau39 <gabriel.moreau@edu.univ-fcomte.fr>
2024-05-26 21:19:34 +0200

    import footer

---
+++ memobook/src/components/FooterPage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    import IconLogo from '@/components/IconLogo.vue';
+    import IconInstagram from '@/components/IconInstagram.vue';
+    import IconFacebook from '@/components/IconFacebook.vue';
+    import IconTwitter from '@/components/IconTwitter.vue';
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
